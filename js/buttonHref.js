b6a59309222cf3ad9b583009e37f5cd0511c25ed
meeyoungchoi <163703903+meeyoungchoi-front-dev@users.noreply.github.com>
2024-04-13 23:42:05 +0900

    게임 미리 해보기 버튼 클릭시 모달 팝업 띄우는 기능 개발

--- js/buttonHref.js
+++ js/buttonHref.js
@@ -1,5 +1,4 @@
 const gamePlayButtonSelector = document.querySelector(".header_game_play_button");
 gamePlayButtonSelector.addEventListener('click', ()=> {
     location.href = "puzzle.html";
-    
 });
